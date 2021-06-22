f3a1b7a883c84e6c6687bcb2f3dd4cef4c624bc8
Spectrum-hue <66886619+Spectrum-hue@users.noreply.github.com>
2021-06-22 16:21:26 +0200

    Update Contractor.java

--- Contractor.java
+++ Contractor.java
@@ -2,20 +2,20 @@
 public class Contractor extends Person {
 
     
-
+    // Constructor and super class
     public Contractor( String name,  String surname, int telephoneNumber,  String emailAddress, String physicalAddress){
     	   super(name, surname, telephoneNumber, emailAddress, physicalAddress);
 
            
 
         } 
-
+        //Customer method with setter
         public void setcustomerInv(){
 
             this.customerInv = customerInv;
 
     }
-    
+    // Override 
     public String toString(){
         String output = "Customer Name: " + name;
         output += "\nPhone Number: " +  telephoneNumber;
