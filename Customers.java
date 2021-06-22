50ae4d7bbaef3d45b0600c54de45099aa01f0426
Spectrum-hue <66886619+Spectrum-hue@users.noreply.github.com>
2021-06-22 16:22:44 +0200

    Update Customers.java

--- Customers.java
+++ Customers.java
@@ -1,20 +1,20 @@
 public class Customers extends Person {
 
     String customerInv;
-
+    // Consructor with super class
     public Customers( String name,  String surname, int telephoneNumber,  String emailAddress, String physicalAddress, String customerInv){
     	   super(name, surname, telephoneNumber, emailAddress, physicalAddress);
 
            this.customerInv = customerInv;
 
         } 
-
+        // Customer method with setter
         public void setcustomerInv(){
 
             this.customerInv = customerInv;
 
     }
-
+    @Override
     public String toString(){
         String output = "Customer Name: " + name;
         output += "\nPhone Number: " +  telephoneNumber;
